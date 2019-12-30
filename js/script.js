0dce09674238d616e0fcfdeba690376ac8d322d3
Jhanjoe Hermano <jhanjoe2002@gmail.com>
2019-12-30 06:28:30 +0000

    Added index styles

--- js/script.js
+++ js/script.js
@@ -7,7 +7,10 @@ var locs = [
   "help",
   "tree-vias",
   "about"
-]
+];
+
+var width = $(window).width();
+var speed = $('.content').data('speed');
 
 function parallax() {
   $('.layer').each(function() {
@@ -20,17 +23,17 @@ $(document).ready(() => {
     e.preventDefault();
 
     if ( Math.abs(dir) > 1) dir = Math.sign(dir);
-    if ( scroll - dir >= 0 ) scroll -= dir;
+    if ( scroll - dir >= 0 && scroll - dir <= (locs.length - 1) * width / speed) scroll -= dir;
 
     parallax();
 
   });
 
-  $(document).on('click', 'a', function(e) {
+  $('nav').on('click', 'a', function(e) {
     loc = $(this).data("loc");
-    scroll = locs.indexOf(loc) * $(window).width() / 60;
+    scroll = locs.indexOf(loc) * width / speed;
 
     parallax();
 
-  })
+  });
 });
